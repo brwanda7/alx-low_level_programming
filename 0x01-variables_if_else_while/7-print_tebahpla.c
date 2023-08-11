da48166c5ca47366f735e37471039d0f5f7f28be
king sharoon <b.kingsharoon@gmail.com>
2023-08-11 18:39:26 +0100

    reverse alphabet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char alphabet[26] = "zyxwvutsrqponmlkjihgfedcba";
+	int i = 0;
 
-	for (i = 26; i > 0; i--)
+	while (i < 26)
 	{
 		putchar(alphabet[i]);
+		i++;
 	}
 	putchar('\n');
 	return (0);
