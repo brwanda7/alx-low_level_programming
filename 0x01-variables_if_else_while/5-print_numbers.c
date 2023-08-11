25c6a7566351ef9e513e963848e1e6b3957f7053
king sharoon <b.kingsharoon@gmail.com>
2023-08-11 18:04:49 +0100

    while loop for 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - how to print 0 to 9 using while 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+
+	return (0);
+}
