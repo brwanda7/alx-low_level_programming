5e4b35a882aeebef38d42b9061330fdeae48e5a6
king sharoon <b.kingsharoon@gmail.com>
2023-08-14 23:09:58 +0100

    kalisa

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,16 @@
- #include "main.h"
+#include "main.h"
 /**
  * main - Prints _putchar
  * Return: 0 always
  */
 int main(void)
 {
-	int n = 0;
-	char y[] = "_putchar";
-
-	while (n <= 7)
-	{
-		_putchar(y[n]);
-		n++;
-	}
-	_putchar('\n');
-	return (0);
+int i;
+char c[] = "_putchar";
+for (i = 0; i <= 7; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
 }
