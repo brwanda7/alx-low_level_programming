60e4136a876a2c5cfa42b86c38fa2ec76c1dbfa8
king sharoon <b.kingsharoon@gmail.com>
2023-08-16 17:37:24 +0100

    push 1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,15 +3,16 @@
 * main - causes an infinite loop
 * Return: 0
 */
-int main(void)
-{	
-int i;
 
+int main(void)
+{
+int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
+
 while (i < 10)
 {
-putchar(i);	
+putchar(i);
 i++;
 }
 printf("Infinite loop avoided! \\o/\n");
