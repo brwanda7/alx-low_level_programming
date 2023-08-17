bd1c3a052b2e07fc330561c0b4b471b20b79ded8
king sharoon <b.kingsharoon@gmail.com>
2023-08-17 19:37:36 +0100

    printing 0 - 9 without 2 - 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
-  
- /**
-  * print_numbers - print 0-9 without 2-4
-  * return: void
-  */
+/**
+ * print_most_numbers - print_most_numbers  0-9 without 2-4
+ * return: void
+ */
 void print_most_numbers(void)
 {
 int a;
 
-a = 0;
-while ( a <= 9)
+a = '0';
+while (a <= '9')
 {
-if (a !=2 && a !=4)
+if (a != '2' && a != '4')
 {
 _putchar(a);
 }
