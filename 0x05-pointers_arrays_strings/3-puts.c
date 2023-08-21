b1b4827ba380584d908397782ce8e7daf43842e9
king sharoon <b.kingsharoon@gmail.com>
2023-08-21 14:10:43 -0700

    put

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts - prints a string, followed b a new line
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
 void _puts(char *str)
@@ -8,6 +8,7 @@ void _puts(char *str)
 while (*str != '\0')
 {
 _putchar(*str++);
-}
+}	
 _putchar('\n');
 }
+
