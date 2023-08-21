e1c4bf6d4988760f323433f353ef20e2ea6dbfe3
king sharoon <b.kingsharoon@gmail.com>
2023-08-21 11:56:51 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int  - updates the value of the variable
+ * @a: pointer to the variable to update
+ * @b: pointer to the variable to update
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
