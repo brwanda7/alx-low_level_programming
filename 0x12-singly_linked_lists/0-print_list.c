ef304e4b2af43db239146d42bc7c10a326cd6e79
king sharoon <b.kingsharoon@gmail.com>
2023-09-21 13:42:06 -0700

    added files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list -  Print elements of a linked list
+ * @h: Pointer to the head of the linked list
+ * Return: Number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
